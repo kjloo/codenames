715254b97d823d82faa8c9e59b5f45b38ee5b64d
kjloo <loo.kaleb@gmail.com>
2023-07-08 12:06:11 -1000

    make a little more tv friendly

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,15 +1,22 @@
 import { useState, FunctionComponent, useEffect } from 'react';
 import axios from 'axios';
-import Card from './Card';
+import { Card, Color } from './Card';
 
 interface BoardProps {
 
 }
 
 const Board: FunctionComponent<BoardProps> = () => {
+    const B_KEY = 66;
+    const C_KEY = 67;
     const F_KEY = 70;
+    const G_KEY = 71;
+    const K_KEY = 75;
+    const R_KEY = 82;
     const [cards, setCards] = useState<string[]>([])
     const [flipImage, setFlipImage] = useState(false);
+    const [marker, setMarker] = useState(Color.BLANK);
+
     const getCards = () => {
         axios.get("/api/board")
             .then(res => {
@@ -19,10 +26,29 @@ const Board: FunctionComponent<BoardProps> = () => {
     }
 
     const handleKeyDown = (event: any) => {
+        if (event.keyCode === B_KEY) {
+            setMarker(Color.BLUE); // Set marker to blue
+        }
+        if (event.keyCode === C_KEY) {
+            setMarker(Color.BLANK); // Clear marker
+        }
         if (event.keyCode === F_KEY) {
             setFlipImage(flipImage => !flipImage); // Toggle the flipImage flag
         }
+        if (event.keyCode === G_KEY) {
+            setMarker(Color.GREY); // Set marker to grey
+        }
+        if (event.keyCode === K_KEY) {
+            setMarker(Color.BLACK); // Set marker to black
+        }
+        if (event.keyCode === R_KEY) {
+            setMarker(Color.RED); // Set marker to red
+        }
     };
+
+    const markerStyle = (): React.CSSProperties => {
+        return marker !== Color.BLANK ? { backgroundColor: marker } : {}
+    }
 
     useEffect(() => {
         getCards();
@@ -33,7 +59,8 @@ const Board: FunctionComponent<BoardProps> = () => {
     }, []);
 
     return <div className='board'>
-        {cards.map(card => <Card key={card} url={card} flipped={flipImage} />)}
+        <div id='marker' style={markerStyle()}></div>
+        {cards.map(card => <Card key={card} url={card} flipped={flipImage} color={marker} />)}
     </div>;
 
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,36 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
+
+export enum Color {
+    BLANK = 'blank',
+    RED = 'red',
+    BLUE = 'blue',
+    BLACK = 'black',
+    GREY = 'grey',
+}
 
 interface CardProps {
     url: string
     flipped: boolean
+    color: Color
 }
 
-const Card: FunctionComponent<CardProps> = ({ url, flipped }) => {
+export const Card: FunctionComponent<CardProps> = ({ url, flipped, color }) => {
 
-    return <div className='card'>
+    const [mark, setMark] = useState(Color.BLANK);
+
+    const handleClick = () => {
+        setMark(color);
+    };
+
+    const cardMarkClass = (): string => {
+        return mark !== Color.BLANK ? 'marked' : ''
+    }
+
+    const cardMarkStyle = (): React.CSSProperties => {
+        return mark !== Color.BLANK ? { backgroundColor: mark } : {}
+    }
+
+    return <div className={`card ${cardMarkClass()}`} onClick={handleClick} style={cardMarkStyle()} >
         <img src={url} className={flipped ? 'flipped-image' : ''} alt="Image" />
     </div>;
-
 }
-
-export default Card;
